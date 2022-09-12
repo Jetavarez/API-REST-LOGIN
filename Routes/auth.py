5eb8433197994b85fd0b13e8f6b7013294b49f40
jetavarez <ing.jatzontavarez@gmail.com>
2022-09-12 00:58:11 -0400

    Se corrigio el login

--- Routes/auth.py
+++ Routes/auth.py
@@ -8,7 +8,7 @@ routes_auth = Blueprint ("routes_auth", __name__ )
 @routes_auth.route("/login", methods=["POST"])
 def login():
     data = request.get_json()
-    if data.username == "Jatzon Tavarez":
+    if data['username'] == "Jatzon Tavarez":
         return write_token(data=request.get_json())
     else:
         response = jsonify({"message" : "User not found"})
